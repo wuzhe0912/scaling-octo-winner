971171e99ae0e74a40e2e91fbd50f265f0993e74
pitt.wu <pitt.wu@jh-tech.tw>
2021-02-02 09:27:10 +0800

    feat：save image

---
+++ src/api/puzzle.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    index: 8,
+    url: '../assets/images/Puzzle/9.png',
+  },
+  {
+    index: 1,
+    url: '../assets/images/Puzzle/2.png',
+  },
+  {
+    index: 3,
+    url: '../assets/images/Puzzle/4.png',
+  },
+  {
+    index: 6,
+    url: '../assets/images/Puzzle/7.png',
+  },
+  {
+    index: 2,
+    url: '../assets/images/Puzzle/3.png',
+  },
+  {
+    index: 4,
+    url: '../assets/images/Puzzle/5.png',
+  },
+  {
+    index: 5,
+    url: '../assets/images/Puzzle/6.png',
+  },
+  {
+    index: 7,
+    url: '../assets/images/Puzzle/8.png',
+  },
+  {
+    index: 0,
+    url: '../assets/images/Puzzle/1.png',
+  },
+];
+
+export default { data };
